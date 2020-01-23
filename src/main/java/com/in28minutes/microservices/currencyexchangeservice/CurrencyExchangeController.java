5ea3a5364b2cefbaf85b5dee394978f3b16c39d1
Alexandra Tincu <tincu.alecsandra@gmail.com>
2020-01-23 14:55:39 +0200

    more spring cloud config - sleuth and zipkin

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.in28minutes.microservices.currencyexchangeservice;
 
 import com.in28minutes.microservices.currencyexchangeservice.bean.ExchangeValue;
 import com.in28minutes.microservices.currencyexchangeservice.repository.ExchangeValueRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,8 @@ import java.util.Objects;
 @RestController
 public class CurrencyExchangeController {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private Environment environment;
 
@@ -23,6 +27,7 @@ public class CurrencyExchangeController {
     public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
 
         ExchangeValue exchangeValue = exchangeValueRepository.findByFromAndTo(from, to);
+        logger.info("{}", exchangeValue);
         exchangeValue.setPort(Integer.parseInt(Objects.requireNonNull(environment.getProperty("local.server.port"))));
 
         return exchangeValue;
